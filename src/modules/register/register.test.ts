dae7c32cdc41ee857494bde98265103b24bd5033
Itay Golan <itaygolan@Itays-MacBook-Pro.local>
2018-07-25 10:33:11 +1200

    fixed testing

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -25,50 +25,57 @@ const mutation = (e: string, p: string) => `
     }
 `;
 
-test("Register user", async () => {
-    // using graphql-request library to make a request to graph ql server
-    const response = await request(getHost(), mutation(email, password)); // request returns a promise
-    expect(response).toEqual({ register: null })
+describe("Register user", async () => {
+    test("check duplicate emails", async () => {
+        // using graphql-request library to make a request to graph ql server
+        const response = await request(getHost(), mutation(email, password)); // request returns a promise
+        expect(response).toEqual({ register: null })
 
-    // test if user was added to DB
-    const users = await User.find({ where: { email } });
-    expect(users).toHaveLength(1);
-    const user = users[0];
-    expect(user.email).toEqual(email);
-    expect(user.password).not.toEqual(password); // because we hased the password
+        // test if user was added to DB
+        const users = await User.find({ where: { email } });
+        expect(users).toHaveLength(1);
+        const user = users[0];
+        expect(user.email).toEqual(email);
+        expect(user.password).not.toEqual(password); // because we hased the password
 
-    // User already made so return error message
-    const response2: any = await request(getHost(), mutation(email, password));
-    expect(response2.register).toHaveLength(1)
-    expect(response2.register[0].path).toEqual("email");
-    expect(response2.register[0].message).toEqual(duplicateEmail);
-
-    // Catch bad/invalid email with valid password
-    const response3: any = await request(getHost(), mutation("bo", password));
-    expect(response3).toEqual({
-        register: [
-            {
-                "message": emailNotLongEnough, 
-                "path": "email"
-            }, 
-            {
-                "message": invalidEmail,
-                "path": "email"
-            }
-        ]
-    });  
+        // test for duplicate email
+        const response2: any = await request(getHost(), mutation(email, password));
+        expect(response2.register).toHaveLength(1)
+        expect(response2.register[0].path).toEqual("email");
+        expect(response2.register[0].message).toEqual(duplicateEmail);
+    });
     
-    // Catch bad/invalid password with valid email
-    const response4: any = await request(getHost(), mutation(email, "bo"));
-    expect(response4).toEqual({
-        register: [
-            {
-                "message": passwordNotLongEnough,
-                "path": "password"
-            }
-        ]
-    })
+    test("check bad email", async () => {
+        // Catch bad/invalid email with valid password
+        const response3: any = await request(getHost(), mutation("bo", password));
+        expect(response3).toEqual({
+            register: [
+                {
+                    "message": emailNotLongEnough, 
+                    "path": "email"
+                }, 
+                {
+                    "message": invalidEmail,
+                    "path": "email"
+                }
+            ]
+        });  
+    });
 
+    test("check bad password", async () => {
+        // Catch bad/invalid password with valid email
+        const response4: any = await request(getHost(), mutation(email, "bo"));
+        expect(response4).toEqual({
+            register: [
+                {
+                    "message": passwordNotLongEnough,
+                    "path": "password"
+                }
+            ]
+        })
+    });
+
+    test("check bad password and bad email", async () => {
         // Catch bad/invalid password with bad/invalid email
         const response5: any = await request(getHost(), mutation("b", "bo"));
         expect(response5).toEqual({
@@ -87,4 +94,5 @@ test("Register user", async () => {
                 }
             ]
         })
+    });
 })
